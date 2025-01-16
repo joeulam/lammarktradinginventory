63c05c6ba73876fd2632d329d4d09d06f3d327ba
joeulam <joeulam345@gmail.com>
2025-01-16 13:40:19 -0500

    Changed API key to be public

--- app/firebase.ts
+++ app/firebase.ts
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  apiKey: "AIzaSyCh2J1qppeeRFbnkISUwWbcaMe-u4OmCCA",
   authDomain: "lammarktrading-3513f.firebaseapp.com",
   projectId: "lammarktrading-3513f",
   storageBucket: "lammarktrading-3513f.firebasestorage.app",
